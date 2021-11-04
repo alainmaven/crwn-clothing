00c8a4246c7009d94d87b9b737b7738c32692abc
Philipp Bastien <philipp.bastien@gsa.gov>
2021-11-04 14:52:13 -0400

    broke homepage into small components and finished adding stylings to menu-item

---
+++ src/components/menu-item/MenuItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./MenuItem.scss";
+
+const MenuItem = ({ title, imageURL, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageURL})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
