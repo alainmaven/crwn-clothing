7c03fd058e6b6c4ae40e0bfe6783834d6bbcc9b2
Alain <pbastien003@gmail.com>
2021-11-10 08:51:53 -0500

    added persistance of storage

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
-<Provider store={store}>
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-</Provider>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
