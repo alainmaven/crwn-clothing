db9b2a63f683173b1a323c048aee015a4b4b4f6d
Philipp Bastien <philipp.bastien@gsa.gov>
2021-11-10 13:18:00 -0500

    added react-stripe-checkout lib and started integration

---
+++ src/components/stripe-button/StripeButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeButton = ({ price }) => {
+
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51JuLF8FMT5QJtvTC' 
+
+}
